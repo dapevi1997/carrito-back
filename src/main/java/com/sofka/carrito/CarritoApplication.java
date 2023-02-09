9e97baf1c92aa6acbb8b5e03e72fec58bdb6d58b
dapevi1997 <dapevi97@gmail.com>
2023-02-09 09:58:47 -0500

    Consumo de API inventario funcionando

--- src/main/java/com/sofka/carrito/CarritoApplication.java
+++ src/main/java/com/sofka/carrito/CarritoApplication.java
@@ -2,9 +2,15 @@ package com.sofka.carrito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CarritoApplication {
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarritoApplication.class, args);
--- src/main/java/com/sofka/carrito/routers/PurchaseRouter.java
+++ src/main/java/com/sofka/carrito/routers/PurchaseRouter.java
@@ -1,8 +1,10 @@
 package com.sofka.carrito.routers;
 
 import com.sofka.carrito.collections.Purchase;
+import com.sofka.carrito.models.Bike;
 import com.sofka.carrito.models.PurchaseDTO;
 import com.sofka.carrito.usecases.AddPurchaseUseCase;
+import com.sofka.carrito.usecases.UpdateBikeInventaryUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -19,8 +21,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -62,6 +63,45 @@ public class PurchaseRouter {
                 POST("/addPurchase").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(PurchaseDTO.class).flatMap(executor)
         );
+    }
+    @RouterOperation(
+            path = "/updateBikeInventary",
+            produces ={
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            method = RequestMethod.PUT,
+            beanClass = PurchaseRouter.class,
+            beanMethod = "updateBikeInventary",
+            operation = @Operation(
+                    operationId = "updateBikeInventary",
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "200",
+                                    description = "successful operation",
+                                    content = @Content(schema = @Schema(
+                                            implementation = String.class
+                                    ))
+                            )
+                    },
+                    requestBody = @RequestBody(
+                            content = @Content(schema = @Schema(
+                                    implementation = Bike.class
+                            ))
+                    )
+            )
+    )
+    @Bean
+    public RouterFunction<ServerResponse> updateBikeInventary(UpdateBikeInventaryUseCase updateBikeInventaryUseCase){
+        Function<Bike, Mono<ServerResponse>> executor = bike -> updateBikeInventaryUseCase.updateBikeInventary(bike)
+                .flatMap(result -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(result));
+
+        return route(
+                PUT("/updateBikeInventary").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(Bike.class).flatMap(executor)
+        );
+
     }
 
 
