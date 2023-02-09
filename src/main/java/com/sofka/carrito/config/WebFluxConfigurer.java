6eeec7453e0ad498e8f7a65fcd6313d6e6a4897a
dapevi1997 <dapevi97@gmail.com>
2023-02-08 23:10:41 -0500

    Cors habilitados

---
+++ src/main/java/com/sofka/carrito/config/WebFluxConfigurer.java
@@ -0,0 +1,21 @@
+package com.sofka.carrito.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+@Configuration
+public class WebFluxConfigurer {
+    @Bean
+    public org.springframework.web.reactive.config.WebFluxConfigurer corsConfigure() {
+        return new WebFluxConfigurerComposite() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
